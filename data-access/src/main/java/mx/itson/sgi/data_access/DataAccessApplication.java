671bd457b9c92691bdc0e005bb6a03aa95c4fbe2
luisa <moraLF20@outlook.com>
2025-03-09 12:34:05 -0700

    proyecto inicial de acceso a datos

---
+++ data-access/src/main/java/mx/itson/sgi/data_access/DataAccessApplication.java
@@ -0,0 +1,13 @@
+package mx.itson.sgi.data_access;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataAccessApplication.class, args);
+	}
+
+}
---
+++ data-access/src/test/java/mx/itson/sgi/data_access/DataAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.itson.sgi.data_access;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
