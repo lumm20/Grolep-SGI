c6f9708c9692550f51391d428b7cdab7868000b4
luisa <moraLF20@outlook.com>
2025-03-19 15:01:03 -0700

    modificando gitignore, y proyectos de negocio y presentacion

---
+++ SGINegocio/src/main/java/controlador/UsuarioControlador.java
@@ -0,0 +1,18 @@
+package controlador;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import mx.itson.sgi.data_access.controllers.UsuarioController;
+import mx.itson.sgi.dto.UsuarioDTO;
+
+@Service
+public class UsuarioControlador {
+
+    @Autowired
+    UsuarioController controller;
+
+    // public boolean login(UsuarioDTO usuario){
+        
+    // }
+}
