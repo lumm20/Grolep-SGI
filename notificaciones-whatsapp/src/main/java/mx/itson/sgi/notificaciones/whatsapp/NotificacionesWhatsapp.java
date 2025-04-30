87e39781f488c2884ee60ef7870a99d09eddedbe
Juvee85 <juventino.lopez248547@potros.itson.edu.mx>
2025-04-29 22:37:11 -0700

    Actualizacion template whatsapp

--- notificaciones-whatsapp/src/main/java/mx/itson/sgi/notificaciones/whatsapp/NotificacionesWhatsapp.java
+++ notificaciones-whatsapp/src/main/java/mx/itson/sgi/notificaciones/whatsapp/NotificacionesWhatsapp.java
@@ -27,12 +27,12 @@ public class NotificacionesWhatsapp {
         try (HttpClient http = HttpClient.newHttpClient()) {
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(new URI("https://graph.facebook.com/v22.0/561402587064064/messages"))
-                    .header("Authorization", "Bearer EAAOSGJzVMCwBO8RH3ZBQuXBn3JzkyD0aoQgMUGhabOhHyyUTcdvBZCsXk7O1gZCUnehdgIoaLypUA1y4RoDIFqe1hbQJZAAhLhE1JLGblo5Tw34ZB1yOQgp57xazQfsj5TkwGHgo7VNyPEyMX25ByZAkWiY7TH5HXrGB8QSxK21dkUrC9nOYQHO8noegxsACylcZCZAVE0YdF2K7GGLmRjZB3VEyLzDHB6g8TwH8ZD")
+                    .header("Authorization", "Bearer EAAOSGJzVMCwBO0gGMZBzCrLQjvyC4eS3tQKBMsg7hTzZB3Kla98YDoNhhRINlZAUolghWy5ZCtYCNCZCVUggYaSHj6IX5wuC9QULsZBVNcodth9z4TdPaYbKFuPiiG25vbKgU4CJ8SZAIZChFk4jqyk2m12jJwmGPjLbBap6lvR60BECZAq5pTroXvXCZBlztEuV8xCHs9WJTX0JyUHs4Ynm08ahj1VHZBVZAWiFtlgZD")
                     .header("Content-Type", "application/json")
                     .POST(HttpRequest.BodyPublishers.ofString(json))
                     .build();
             HttpResponse<String> response = http.send(request, BodyHandlers.ofString());
-            
+
             logger.log(Level.INFO, json);
             logger.log(Level.INFO, response.body());
         } catch (URISyntaxException | IOException | InterruptedException e) {
@@ -42,24 +42,63 @@ public class NotificacionesWhatsapp {
 
     public String crearJSON(String folio, String monto, String conceptos, String nombreAlumno, String descuento, String subtotal, String metodoPago, String numero) {
         Map<String, Object> mensaje = new HashMap<>();
-        Map<String, Object> contenido = new HashMap<>();
 
-        String texto = "Pago Recibido \n----------------------\nFolio del pago: " + folio + "\nAlumno: " + nombreAlumno  + "\nConceptos pagados: "
-                + conceptos  + "\nMétodo de pago: " + metodoPago + "\n----------------------\nSubtotal: " + subtotal + "\nMonto total: " + monto + "\nDescuento aplicado: " + descuento;
+        Map<String, Object> template = new HashMap<>();
+        Map<String, Object> language = new HashMap<>();
 
-        contenido.put("preview_url", "false");
-        contenido.put("body", texto);
+        ArrayList<Map<String, Object>> components = new ArrayList<>();
+
+        // Llena información para mensajes
+        ArrayList<Map<String, String>> parameters = new ArrayList<>();
+        Map<String, String> folioMap = new HashMap<>();
+        folioMap.put("type", "text");
+        folioMap.put("text", folio);
+        Map<String, String> nombreAlumnoMap = new HashMap<>();
+        nombreAlumnoMap.put("type", "text");
+        nombreAlumnoMap.put("text", nombreAlumno);
+        Map<String, String> conceptoMap = new HashMap<>();
+        conceptoMap.put("type", "text");
+        conceptoMap.put("text", conceptos);
+        Map<String, String> metodoMap = new HashMap<>();
+        metodoMap.put("type", "text");
+        metodoMap.put("text", metodoPago);
+        Map<String, String> subtotalMap = new HashMap<>();
+        subtotalMap.put("type", "text");
+        subtotalMap.put("text", subtotal);
+        Map<String, String> montoMap = new HashMap<>();
+        montoMap.put("type", "text");
+        montoMap.put("text", monto);
+        Map<String, String> descuentoMap = new HashMap<>();
+        descuentoMap.put("type", "text");
+        descuentoMap.put("text", descuento);
+        parameters.add(folioMap);
+        parameters.add(nombreAlumnoMap);
+        parameters.add(conceptoMap);
+        parameters.add(metodoMap);
+        parameters.add(subtotalMap);
+        parameters.add(montoMap);
+        parameters.add(descuentoMap);
+
+        // Configuración de template
+        Map<String, Object> body = new HashMap<>();
+        body.put("type", "body");
+        body.put("parameters", parameters);
+        components.add(body);
+        language.put("code", "es_MX");
+        template.put("name", "confirmacion_de_pago");
+        template.put("language", language);
+        template.put("components", components);
 
         mensaje.put("messaging_product", "whatsapp");
         mensaje.put("recipient_type", "individual");
         mensaje.put("to", "+52" + numero);
-        mensaje.put("type", "text");
-        mensaje.put("text", contenido);
+        mensaje.put("type", "template");
+        mensaje.put("template", template);
         Gson gson = new Gson();
 
         return gson.toJson(mensaje);
     }
-    
+
     public void enviarNotificacion(String folio, String costo, String conceptos, String nombreAlumno, String subtotal, String descuento, String metodoPago, String numero) {
         enviarPeticion(crearJSON(folio, costo, conceptos, nombreAlumno, descuento, subtotal, metodoPago, numero));
     }
