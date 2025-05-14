a7e1b4da09c04ac1929e181b55b51a3af29c24ea
Miguel Rodriguez <116122710+JoMi21@users.noreply.github.com>
2025-05-14 04:50:23 -0700

    detalles casi finales

--- SGIDTO/src/main/java/mx/itson/sgi/dto/MetodosPagoDTO.java
+++ SGIDTO/src/main/java/mx/itson/sgi/dto/MetodosPagoDTO.java
@@ -4,4 +4,18 @@ public enum MetodosPagoDTO {
     Transferencia,
     Efectivo,
     Tarjeta,;
+
+
+    public static MetodosPagoDTO fromString(String value) {
+        switch (value.toUpperCase()) {
+            case "TRANSFERENCIA":
+                return Transferencia;
+            case "EFECTIVO":
+                return Efectivo;
+            case "TARJETA":
+                return Tarjeta;
+            default:
+                throw new IllegalArgumentException("Método de pago no válido: " + value);
+        }
+    }
 }
