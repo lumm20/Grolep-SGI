e04c2d008d13e374a5cbb799bbb9bcae9c23be5f
Miguel Rodriguez <116122710+JoMi21@users.noreply.github.com>
2025-05-15 03:03:41 -0700

    todos los endpoints ruteados y funcionando

--- data-access/src/main/java/mx/itson/sgi/data_access/entities/Alumno.java
+++ data-access/src/main/java/mx/itson/sgi/data_access/entities/Alumno.java
@@ -82,7 +82,7 @@ public class Alumno {
     @AttributeOverride(name = "tipo", column = @Column(name = "tipo_beca"))
     private Beca beca;
 
-    @OneToMany(mappedBy = "alumno", cascade = {CascadeType.ALL}, orphanRemoval = true)
+    @OneToMany(mappedBy = "alumno", cascade = {CascadeType.ALL}, orphanRemoval = false)
     private List<Cuota> cuotas;
     
     @OneToMany(mappedBy = "alumno", cascade = CascadeType.ALL)
